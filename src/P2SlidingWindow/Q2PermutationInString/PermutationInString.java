4d09724fefbf2bbda09c8c988c2446aedc12aa94
AdityaDudhal <a.dudhal@maxxton.com>
2025-05-21 17:17:41 +0530

    refactor PermutationInString class to improve substring check logic and enhance readability

--- src/P2SlidingWindow/Q2PermutationInString/PermutationInString.java
+++ src/P2SlidingWindow/Q2PermutationInString/PermutationInString.java
@@ -10,17 +10,41 @@ public class PermutationInString {
       s1Map.put(s1.charAt(i), s1Map.getOrDefault(s1.charAt(i), 0) + 1);
     }
 
-    int left = 0;
-    int right = 0;
-    for (right = 0; right < s2.length(); right++) {
-      while (!s1Map.containsKey(s2.charAt(right))) {
-        left++;
+    int ptr = 0;
+    for (ptr = 0; ptr < s2.length(); ptr++) {
+      if (s1Map.containsKey(s2.charAt(ptr))) {
+        break;
       }
     }
+    if (ptr == s2.length()) {
+      return false;
+    }
 
+    int left = ptr;
+    int right = ptr;
+
+    for (; right < s2.length(); right++) {
+      if (s1Map.containsKey(s2.charAt(right))) {
+        while (s2Map.get(s2.charAt(right)) > s1Map.get(s1.charAt(right))) {
+          s2Map.put(s2.charAt(left), s2Map.get(s2.charAt(left)) - 1);
+          left++;
+        }
+      }
+      else {
+        if (s1Map.equals(s2Map)) {
+          return true;
+        }
+        s2Map.clear();
+      }
+    }
+    return false;
   }
 
   public static void main(String[] args) {
-
+    //    String s1 = "ab", s2 = "eidbaooo";
+    //
+    String s1 = "ab", s2 = "eidboaoo";
+    PermutationInString permutationInString = new PermutationInString();
+    System.out.println(permutationInString.checkInclusion(s1, s2));
   }
 }
