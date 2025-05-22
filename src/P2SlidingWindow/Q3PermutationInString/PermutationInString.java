1660af444a35f933345231f72ef1a1ab4c77ab1c
AdityaDudhal <a.dudhal@maxxton.com>
2025-05-22 09:45:32 +0530

    add documentation to PermutationInString class with examples and constraints

--- src/P2SlidingWindow/Q3PermutationInString/PermutationInString.java
+++ src/P2SlidingWindow/Q3PermutationInString/PermutationInString.java
@@ -2,6 +2,31 @@ package P2SlidingWindow.Q3PermutationInString;
 
 import java.util.HashMap;
 //https://leetcode.com/problems/permutation-in-string/description/?envType=problem-list-v2&envId=sliding-window
+
+/**
+ * Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+ *
+ * In other words, return true if one of s1's permutations is the substring of s2.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s1 = "ab", s2 = "eidbaooo"
+ * Output: true
+ * Explanation: s2 contains one permutation of s1 ("ba").
+ * Example 2:
+ *
+ * Input: s1 = "ab", s2 = "eidboaoo"
+ * Output: false
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s1.length, s2.length <= 104
+ * s1 and s2 consist of lowercase English letters.
+ */
+
 public class PermutationInString {
   public boolean checkInclusion(String s1, String s2) {
     HashMap<Character, Integer> s1Map = new HashMap<>();
