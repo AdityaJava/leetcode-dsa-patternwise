524288dc485938aff3e9e437f2c43c51d8024e95
AdityaDudhal <a.dudhal@maxxton.com>
2025-06-02 13:58:14 +0530

    add NumberOfIslands and PathSum classes for counting islands and checking path sums in binary trees

---
+++ src/P4DFS/Q1PathSum/PathSum.java
@@ -0,0 +1,60 @@
+package P4DFS.Q1PathSum;
+
+public class PathSum {
+  public boolean hasPathSum(TreeNode root, int targetSum) {
+    if (root == null) {
+      return false;
+    }
+    return checkPathSumRecursively(root, targetSum, 0);
+  }
+
+  private boolean checkPathSumRecursively(TreeNode node, int targetSum, int sum) {
+    if (node != null) {
+      sum = sum + node.val;
+    }
+    if (node != null && node.left == null && node.right == null) {
+      return sum == targetSum;
+    }
+    else if (node != null) {
+      boolean result = false;
+      result = checkPathSumRecursively(node.left, targetSum, sum);
+      if (result) {
+        return true;
+      }
+      return checkPathSumRecursively(node.right, targetSum, sum);
+    }
+    return false;
+  }
+
+  public static void main(String[] args) {
+    //    TreeNode rootNode = new TreeNode(5);
+    //    TreeNode node1 = new TreeNode(4);
+    //    TreeNode node2 = new TreeNode(8);
+    //
+    //    rootNode.left = node1;
+    //    rootNode.right = node2;
+    //
+    //    TreeNode node3 = new TreeNode(11);
+    //    node1.left = node3;
+    //
+    //    TreeNode node4 = new TreeNode(13);
+    //    TreeNode node5 = new TreeNode(4);
+    //    node2.left = node4;
+    //    node2.right = node5;
+    //
+    //    TreeNode node6 = new TreeNode(7);
+    //    TreeNode node7 = new TreeNode(2);
+    //    node3.left = node6;
+    //    node3.right = node7;
+    //
+    //    TreeNode node8 = new TreeNode(1);
+    //    node5.right = node8;
+
+    TreeNode rootNode = new TreeNode(1);
+    TreeNode node1 = new TreeNode(2);
+    rootNode.left = node1;
+
+    PathSum pathSum = new PathSum();
+    System.out.println(pathSum.checkPathSumRecursively(rootNode, 0, 0));
+  }
+}
---
+++ src/P4DFS/Q2NumberOfIslands/NumberOfIslands.java
@@ -0,0 +1,45 @@
+package P4DFS.Q2NumberOfIslands;
+
+public class NumberOfIslands {
+
+  public int numIslands(char[][] grid) {
+    int[][] visited = new int[grid.length][grid[0].length];
+    int count = 0;
+    for (int row = 0; row < grid.length; row++) {
+      for (int column = 0; column < grid[row].length; column++) {
+        if (grid[row][column] == '1' && visited[row][column] == 0) {
+          dfs(row, column, visited, grid);
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+
+  private void dfs(int row, int colum, int[][] visited, char[][] grid) {
+    visited[row][colum] = 1;
+    //right, left, up, down
+    int[] directionX = { 0, 0, -1, 1 };
+    int[] directionY = { 1, -1, 0, 0 };
+    visited[row][colum] = 1;
+    for (int direction = 0; direction < 4; direction++) {
+      int currRow = row - directionX[direction];
+      int currCol = colum - directionY[direction];
+      if (isPointValid(currRow, currCol, visited, grid)) {
+        dfs(currRow, currCol, visited, grid);
+      }
+    }
+  }
+
+  private boolean isPointValid(int currRow, int currColumn, int[][] visited, char[][] grid) {
+    return currRow >= 0 && currRow < visited.length && currColumn >= 0 && currColumn < visited[0].length && visited[currRow][currColumn] == 0 && grid[currRow][currColumn] == '1';
+  }
+
+  public static void main(String[] args) {
+    //char[][] grid = { { '1', '1', '1', '1', '0' }, { '1', '1', '0', '1', '0' }, { '1', '1', '0', '0', '0' }, { '0', '0', '0', '0', '0' } };
+    char[][] grid = { { '1', '1', '0', '0', '0' }, { '1', '1', '0', '0', '0' }, { '0', '0', '1', '0', '0' }, { '0', '0', '0', '1', '1' } };
+
+    NumberOfIslands numberOfIslands = new NumberOfIslands();
+    System.out.println(numberOfIslands.numIslands(grid));
+  }
+}
