108427aab006a9ebe28285d3571ccbae7776d667
AdityaDudhal <a.dudhal@maxxton.com>
2025-06-17 18:56:25 +0530

    add FibonacciSeries and PerfectSquares classes for calculating Fibonacci numbers using Dynamic Programming and perfect square counts

---
+++ src/DynamicProgramming/Q1FibonacciSeries/FibonacciSeries.java
@@ -0,0 +1,26 @@
+package DynamicProgramming.Q1FibonacciSeries;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FibonacciSeries {
+
+  private int fibonacci(int n, List<Integer> dp) {
+    if (n <= 1) {
+      return 1;
+    }
+    if (dp.get(n) != -1) {
+      return dp.get(n);
+    }
+    dp.set(n, fibonacci(n - 1, dp) + fibonacci(n - 2, dp));
+    return dp.get(n);
+  }
+
+  public static void main(String[] args) {
+    int n = 5;
+    FibonacciSeries fibonacciSeries = new FibonacciSeries();
+    List<Integer> dp = new ArrayList<>(Collections.nCopies(n + 1, -1));
+    System.out.println(fibonacciSeries.fibonacci(n, dp));
+  }
+}
---
+++ src/P5BFS/Q4PerfectSquares/PerfectSquares.java
@@ -0,0 +1,14 @@
+package P5BFS.Q4PerfectSquares;
+
+public class PerfectSquares {
+  public int numSquares(int n) {
+    int i = 1;
+    int count = 0;
+
+    return count;
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
