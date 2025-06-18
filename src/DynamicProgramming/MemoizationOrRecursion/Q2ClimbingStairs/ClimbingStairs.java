36a749979d9dde7d37c3f21950e06a77a85e6cdc
AdityaDudhal <a.dudhal@maxxton.com>
2025-06-18 16:39:33 +0530

    add ClimbingStairs class for calculating the number of ways to climb stairs using dynamic programming

---
+++ src/DynamicProgramming/MemoizationOrRecursion/Q2ClimbingStairs/ClimbingStairs.java
@@ -0,0 +1,31 @@
+package DynamicProgramming.MemoizationOrRecursion.Q2ClimbingStairs;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ClimbingStairs {
+  public int climbStairs(int n) {
+    List<Integer> dp = new ArrayList<>(Collections.nCopies(n + 1, -1));
+    return dfs(n, dp);
+  }
+
+  public int dfs(int n, List<Integer> dp) {
+    if (n <= 1) {
+      return 1;
+    }
+    if (dp.get(n) != -1) {
+      return dp.get(n);
+    }
+    int left = dfs(n - 1, dp);
+    int right = dfs(n - 2, dp);
+    dp.set(n, left + right);
+    return dp.get(n);
+  }
+
+  public static void main(String[] args) {
+    int n = 45;
+    ClimbingStairs climbingStairs = new ClimbingStairs();
+    System.out.println(climbingStairs.climbStairs(n));
+  }
+}
