082cb12aea57246140f7815f66d50bbcacd7ef7c
AdityaDudhal <a.dudhal@maxxton.com>
2025-06-25 08:26:51 +0530

    optimize frogJump method in FrogJumWithKDistances class by implementing memoization for improved performance

--- src/P6DynamicProgramming/MemoizationOrRecursion/Q4FrogJumWithKDistances/FrogJumWithKDistances.java
+++ src/P6DynamicProgramming/MemoizationOrRecursion/Q4FrogJumWithKDistances/FrogJumWithKDistances.java
@@ -1,21 +1,29 @@
 package P6DynamicProgramming.MemoizationOrRecursion.Q4FrogJumWithKDistances;
 
+import java.util.Arrays;
+
 public class FrogJumWithKDistances {
   public int frogJump(int[] heights, int k) {
-    return dfs(heights, k, heights.length - 1);
+    int[] dp = new int[heights.length];
+    Arrays.fill(dp, -1);
+    return dfs(heights, k, heights.length - 1, dp);
   }
 
-  private int dfs(int[] heights, int k, int currentStep) {
+  private int dfs(int[] heights, int k, int currentStep, int[] dp) {
     if (currentStep == 0) {
       return 0;
+    }
+    if (dp[currentStep] != -1) {
+      return dp[currentStep];
     }
     int min = Integer.MAX_VALUE;
     for (int i = k; i > 0; i--) {
       if ((currentStep - i) >= 0) {
-        min = Math.min(Math.abs(heights[currentStep] - heights[currentStep - i]) + dfs(heights, k, currentStep - i), min);
+        min = Math.min(Math.abs(heights[currentStep] - heights[currentStep - i]) + dfs(heights, k, currentStep - i, dp), min);
       }
     }
-    return min;
+    dp[currentStep] = min;
+    return dp[currentStep];
   }
 
   public static void main(String[] args) {
