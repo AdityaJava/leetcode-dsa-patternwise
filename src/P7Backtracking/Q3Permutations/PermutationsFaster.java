86f8e7ba119edc511eaa9605f0d428d022f9ae71
AdityaDudhal <a.dudhal@maxxton.com>
2025-07-01 10:21:38 +0530

    add PermutationsFaster class with optimized DFS implementation for generating permutations

--- src/P7Backtracking/Q3Permutations/PermutationsFaster.java
+++ src/P7Backtracking/Q3Permutations/PermutationsFaster.java
@@ -4,36 +4,38 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Permutations {
+public class PermutationsFaster {
   public List<List<Integer>> permute(int[] nums) {
     List<List<Integer>> answers = new ArrayList<>();
     List<Integer> answer = new ArrayList<>();
     List<Boolean> visited = new ArrayList<>(Collections.nCopies(nums.length, false));
-    dfs(0, nums, answers, answer, visited);
+    for (int i = 0; i < nums.length; i++) {
+      dfs(i, nums, answers, answer, visited);
+    }
     return answers;
   }
 
   private void dfs(int index, int[] nums, List<List<Integer>> answers, List<Integer> answer, List<Boolean> visited) {
+    visited.set(index, true);
+    answer.add(nums[index]);
     if (answer.size() == nums.length) {
       answers.add(new ArrayList<>(answer));
+      answer.removeLast();
+      visited.set(index, false);
       return;
     }
     for (int i = 0; i < nums.length; i++) {
       if (!visited.get(i)) {
-        visited.set(i, true);
-        answer.add(nums[i]);
-
         dfs(i, nums, answers, answer, visited);
-
-        visited.set(i, false);
-        answer.removeLast();
       }
     }
+    visited.set(index, false);
+    answer.removeLast();
   }
 
   public static void main(String[] args) {
     int[] nums = { 1, 2, 3 };
-    Permutations permutations = new Permutations();
-    permutations.permute(nums).forEach(System.out::println);
+    PermutationsFaster permutationsFaster = new PermutationsFaster();
+    permutationsFaster.permute(nums).forEach(System.out::println);
   }
 }
