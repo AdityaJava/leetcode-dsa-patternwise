92a12d3015aa23c2998ac35224ce938f622b15c9
AdityaDudhal <a.dudhal@maxxton.com>
2025-07-10 11:37:37 +0530

    add Heapify class to implement max heapify algorithm

---
+++ src/P8HeapORPriorityQueue/heapify/Heapify.java
@@ -0,0 +1,40 @@
+package P8HeapORPriorityQueue.heapify;
+
+import java.util.Arrays;
+
+public class Heapify {
+
+    private void heapifyToMaxHeap(int[] arr, int index) {
+        int largestIndex = index;
+        int leftChildIndex = 2 * index + 1;
+        int rightChildIndex = 2 * index + 2;
+
+        if (leftChildIndex < arr.length && arr[leftChildIndex] > arr[largestIndex]) {
+            largestIndex = leftChildIndex;
+        }
+        if (rightChildIndex < arr.length && arr[rightChildIndex] > arr[largestIndex]) {
+            largestIndex = rightChildIndex;
+        }
+
+        if (largestIndex != index) {
+            swap(arr, index, largestIndex);
+            heapifyToMaxHeap(arr, largestIndex);
+        }
+    }
+
+    private void swap(int[] arr, int index1, int index2) {
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 15, 30, 40, 50, 100, 25, 45, 60};
+        Heapify heapify = new Heapify();
+        for (int i = arr.length / 2 - 1; i >= 0; i--) {
+            heapify.heapifyToMaxHeap(arr, i);
+        }
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+
+}
