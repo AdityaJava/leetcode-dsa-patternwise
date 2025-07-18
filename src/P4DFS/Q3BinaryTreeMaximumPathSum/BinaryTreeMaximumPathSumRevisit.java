9bca55a152d7cde641e21dca4606f5696ace8a06
AdityaDudhal <a.dudhal@maxxton.com>
2025-07-18 10:04:23 +0530

    refactor BinaryTreeMaximumPathSumRevisit for clarity and correctness in max path calculation

--- src/P4DFS/Q3BinaryTreeMaximumPathSum/BinaryTreeMaximumPathSumRevisit.java
+++ src/P4DFS/Q3BinaryTreeMaximumPathSum/BinaryTreeMaximumPathSumRevisit.java
@@ -6,36 +6,34 @@ import P4DFS.TreeNode;
 
 //Not working
 public class BinaryTreeMaximumPathSumRevisit {
+  int max = Integer.MIN_VALUE;
 
   public int maxPathSum(TreeNode root) {
-    return recursiveMaxPath(root);
+    recursiveMaxPath(root);
+    return max;
   }
 
   public int recursiveMaxPath(TreeNode node) {
     if (node == null) {
       return 0;
     }
-
-    int leftSum = recursiveMaxPath(node.left);
-    int rightSum = recursiveMaxPath(node.right);
-    int max1 = Math.max(node.val, Math.max(leftSum, rightSum));
-
-    int max2 = Math.max(node.val + rightSum, node.val + leftSum);
-    int sumAll = node.val + leftSum + rightSum;
-
-    return Math.max(sumAll, Math.max(max1, max2));
+    int left = Math.max(recursiveMaxPath(node.left), 0);
+    int right = Math.max(recursiveMaxPath(node.right), 0);
+    int max1 = Math.max(left, right);
+    max = Math.max(max, Math.max(node.val + max1, node.val + right + left));
+    return node.val + max1;
   }
 
   public static void main(String[] args) {
-    //    TreeNode root = new TreeNode(1);
-    //    root.left = new TreeNode(-2);
-    //    root.right = new TreeNode(3);
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(-2);
+    root.right = new TreeNode(3);
 
     //    TreeNode root = new TreeNode(1);
     //    root.left = new TreeNode(2);
     //    root.right = new TreeNode(3);
 
-    TreeNode root = new TreeNode(-3);
+    //    TreeNode root = new TreeNode(-3);
 
     BinaryTreeMaximumPathSumRevisit revisit = new BinaryTreeMaximumPathSumRevisit();
     System.out.println(revisit.maxPathSum(root));
