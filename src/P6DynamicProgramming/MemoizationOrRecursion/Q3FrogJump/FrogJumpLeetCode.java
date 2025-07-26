866e0814afcaf1bae7f4aabef504b2cc0ffb095d
AdityaDudhal <a.dudhal@maxxton.com>
2025-07-26 11:04:53 +0530

    refactor FrogJumpLeetCode to improve initial stone validation and implement memoization in DFS

--- src/P6DynamicProgramming/MemoizationOrRecursion/Q3FrogJump/FrogJumpLeetCode.java
+++ src/P6DynamicProgramming/MemoizationOrRecursion/Q3FrogJump/FrogJumpLeetCode.java
@@ -9,11 +9,10 @@ import java.util.stream.Collectors;
 //https://leetcode.com/problems/frog-jump/description/
 public class FrogJumpLeetCode {
   public boolean canCross(int[] stones) {
-    if (stones.length == 2) {
-      if (stones[0] + 1 == stones[1]) {
-        return true;
+    if (stones.length >= 2) {
+      if (stones[0] + 1 != stones[1]) {
+        return false;
       }
-      return false;
     }
     Set<Integer> stoneSet = Arrays.stream(stones).boxed().collect(Collectors.toSet());
     Map<String, Boolean> dp = new HashMap<>();
@@ -22,6 +21,9 @@ public class FrogJumpLeetCode {
 
   private boolean dfs(Set<Integer> stoneSet, int stepCount, int currentStep, int lastStep, Map<String, Boolean> dp) {
     String key = stepCount + "" + currentStep;
+    if (dp.containsKey(key)) {
+      return dp.get(key);
+    }
     if (currentStep == lastStep) {
       return true;
     }
@@ -29,27 +31,32 @@ public class FrogJumpLeetCode {
     if ((stepCount - 1) > 0 && stoneSet.contains(currentStep + (stepCount - 1))) {
       result = dfs(stoneSet, stepCount - 1, currentStep + (stepCount - 1), lastStep, dp);
       if (result) {
+        dp.put(key, true);
         return true;
       }
     }
     if (stoneSet.contains(currentStep + stepCount)) {
       result = dfs(stoneSet, stepCount, currentStep + stepCount, lastStep, dp);
       if (result) {
+        dp.put(key, true);
         return true;
       }
     }
     if (stoneSet.contains(currentStep + stepCount + 1)) {
       result = dfs(stoneSet, stepCount + 1, currentStep + stepCount + 1, lastStep, dp);
       if (result) {
+        dp.put(key, true);
         return true;
       }
     }
+    dp.put(key, false);
     return false;
   }
 
   public static void main(String[] args) {
     //    int[] stones = { 0, 1, 3, 5, 6, 8, 12, 17 };
-    int[] stones = { 0, 2 };
+    int[] stones = { 0, 2, 4, 5, 6, 8, 9, 11, 14, 17, 18, 19, 20, 22, 23, 24, 25, 27, 30 };
+    //int[] stones = { 0, 2 };
     FrogJumpLeetCode frogJumpLeetCode = new FrogJumpLeetCode();
     System.out.println(frogJumpLeetCode.canCross(stones));
   }
