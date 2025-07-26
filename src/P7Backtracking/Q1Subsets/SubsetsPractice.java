3b93f341739571c800beff34c3c3f05608284aa2
AdityaDudhal <a.dudhal@maxxton.com>
2025-07-26 17:58:33 +0530

    add SubsetsPractice class with DFS implementation for generating subsets

---
+++ src/P7Backtracking/Q1Subsets/SubsetsPractice.java
@@ -0,0 +1,31 @@
+package P7Backtracking.Q1Subsets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubsetsPractice {
+  public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> answers = new ArrayList<>();
+    List<Integer> answer = new ArrayList<>();
+    dfs(nums, 0, answers, answer);
+    return answers;
+  }
+
+  private void dfs(int[] nums, int index, List<List<Integer>> answers, List<Integer> answer) {
+    if (index >= nums.length) {
+      return;
+    }
+    answer.add(nums[index]);
+    answers.add(new ArrayList<>(answer));
+    for (int i = index; i < nums.length; i++) {
+      dfs(nums, i + 1, answers, answer);
+    }
+    answer.removeLast();
+  }
+
+  public static void main(String[] args) {
+    int[] nums = { 1, 2, 3 };
+    SubsetsPractice subsetsPractice = new SubsetsPractice();
+    subsetsPractice.subsets(nums).forEach(System.out::println);
+  }
+}
