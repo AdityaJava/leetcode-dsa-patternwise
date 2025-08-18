05a9fb1f65c36c75f9e144bab783781e7f234c1c
AdityaDudhal <a.dudhal@maxxton.com>
2025-08-18 18:33:13 +0530

    add TrionicArray class with initial implementation for checking trionic arrays

---
+++ src/contests/trionicarray/TrionicArray.java
@@ -0,0 +1,14 @@
+package contests.trionicarray;
+
+public class TrionicArray {
+
+  public boolean isTrionic(int[] nums) {
+    int p = 1, q = p + 1;
+
+  }
+
+
+  public static void main(String[] args) {
+
+  }
+}
