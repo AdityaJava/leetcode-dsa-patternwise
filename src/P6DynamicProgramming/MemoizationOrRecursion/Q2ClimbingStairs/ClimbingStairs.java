d452113bba6db290bc27e690463de819d651865d
aditya-cresults <adudhalcresults@gmail.com>
2026-01-22 16:09:53 +0530

    clear code

--- src/P6DynamicProgramming/MemoizationOrRecursion/Q2ClimbingStairs/ClimbingStairs.java
+++ src/P6DynamicProgramming/MemoizationOrRecursion/Q2ClimbingStairs/ClimbingStairs.java
@@ -8,20 +8,20 @@ import java.util.List;
 
 /**
  * You are climbing a staircase. It takes n steps to reach the top.
- *
+ * <p>
  * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
- *
- *
- *
+ * <p>
+ * <p>
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: n = 2
  * Output: 2
  * Explanation: There are two ways to climb to the top.
  * 1. 1 step + 1 step
  * 2. 2 steps
  * Example 2:
- *
+ * <p>
  * Input: n = 3
  * Output: 3
  * Explanation: There are three ways to climb to the top.
@@ -30,27 +30,45 @@ import java.util.List;
  * 3. 2 steps + 1 step
  */
 public class ClimbingStairs {
-  public int climbStairs(int n) {
-    List<Integer> dp = new ArrayList<>(Collections.nCopies(n + 1, -1));
-    return dfs(n, dp);
-  }
-
-  public int dfs(int n, List<Integer> dp) {
-    if (n <= 1) {
-      return 1;
+    public int climbStairs(int n) {
+        List<Integer> dp = new ArrayList<>(Collections.nCopies(n + 1, -1));
+        int[] ans = new int[1];
+        dfs2(0, dp, n);
+        System.out.println(ans[0]);
+        return dfs(n, dp);
     }
-    if (dp.get(n) != -1) {
-      return dp.get(n);
-    }
-    int left = dfs(n - 1, dp);
-    int right = dfs(n - 2, dp);
-    dp.set(n, left + right);
-    return dp.get(n);
-  }
 
-  public static void main(String[] args) {
-    int n = 45;
-    ClimbingStairs climbingStairs = new ClimbingStairs();
-    System.out.println(climbingStairs.climbStairs(n));
-  }
+    public int dfs(int n, List<Integer> dp) {
+        if (n <= 1) {
+            return 1;
+        }
+        if (dp.get(n) != -1) {
+            return dp.get(n);
+        }
+        int left = dfs(n - 1, dp);
+        int right = dfs(n - 2, dp);
+        dp.set(n, left + right);
+        return dp.get(n);
+    }
+
+    public int dfs2(int point, List<Integer> dp, int n) {
+        if (point > n) {
+            return 0;
+        }
+        if (point == n) {
+            return 1;
+        }
+        if (dp.get(point) != -1) {
+            return dp.get(point);
+        }
+        dp.set(point, dfs2(point + 1, dp, n)
+                + dfs2(point + 2, dp, n));
+        return dp.get(point);
+    }
+
+    public static void main(String[] args) {
+        int n = 44;
+        ClimbingStairs climbingStairs = new ClimbingStairs();
+        System.out.println(climbingStairs.climbStairs(n));
+    }
 }
--- src/P6DynamicProgramming/MemoizationOrRecursion/Q3FrogJump/FrogJumpLeetCode.java
+++ src/P6DynamicProgramming/MemoizationOrRecursion/Q3FrogJump/FrogJumpLeetCode.java
@@ -8,67 +8,67 @@ import java.util.stream.Collectors;
 
 //https://leetcode.com/problems/frog-jump/description/
 public class FrogJumpLeetCode {
-  public boolean canCross(int[] stones) {
-    if (stones.length >= 2) {
-      if (stones[0] + 1 != stones[1]) {
+    public boolean canCross(int[] stones) {
+        if (stones.length >= 2) {
+            if (stones[0] + 1 != stones[1]) {
+                return false;
+            }
+        }
+        Set<Integer> stoneSet = Arrays.stream(stones).boxed().collect(Collectors.toSet());
+        Map<String, Boolean> dp = new HashMap<>();
+        return dfsCodeRefined(stoneSet, 1, stones[1], stones[stones.length - 1], dp);
+    }
+
+    private boolean dfs(Set<Integer> stoneSet, int stepCount, int currentStep, int lastStep, Map<String, Boolean> dp) {
+        String key = stepCount + "" + currentStep;
+        if (dp.containsKey(key)) {
+            return dp.get(key);
+        }
+        if (currentStep == lastStep) {
+            return true;
+        }
+        boolean result = false;
+        if ((stepCount - 1) > 0 && stoneSet.contains(currentStep + (stepCount - 1))) {
+            result = dfs(stoneSet, stepCount - 1, currentStep + (stepCount - 1), lastStep, dp);
+            if (result) {
+                dp.put(key, true);
+                return true;
+            }
+        }
+        if (stoneSet.contains(currentStep + stepCount)) {
+            result = dfs(stoneSet, stepCount, currentStep + stepCount, lastStep, dp);
+            if (result) {
+                dp.put(key, true);
+                return true;
+            }
+        }
+        if (stoneSet.contains(currentStep + stepCount + 1)) {
+            result = dfs(stoneSet, stepCount + 1, currentStep + stepCount + 1, lastStep, dp);
+            if (result) {
+                dp.put(key, true);
+                return true;
+            }
+        }
+        dp.put(key, false);
         return false;
-      }
     }
-    Set<Integer> stoneSet = Arrays.stream(stones).boxed().collect(Collectors.toSet());
-    Map<String, Boolean> dp = new HashMap<>();
-    return dfsCodeRefined(stoneSet, 1, stones[1], stones[stones.length - 1], dp);
-  }
 
-  private boolean dfs(Set<Integer> stoneSet, int stepCount, int currentStep, int lastStep, Map<String, Boolean> dp) {
-    String key = stepCount + "" + currentStep;
-    if (dp.containsKey(key)) {
-      return dp.get(key);
-    }
-    if (currentStep == lastStep) {
-      return true;
-    }
-    boolean result = false;
-    if ((stepCount - 1) > 0 && stoneSet.contains(currentStep + (stepCount - 1))) {
-      result = dfs(stoneSet, stepCount - 1, currentStep + (stepCount - 1), lastStep, dp);
-      if (result) {
-        dp.put(key, true);
-        return true;
-      }
-    }
-    if (stoneSet.contains(currentStep + stepCount)) {
-      result = dfs(stoneSet, stepCount, currentStep + stepCount, lastStep, dp);
-      if (result) {
-        dp.put(key, true);
-        return true;
-      }
-    }
-    if (stoneSet.contains(currentStep + stepCount + 1)) {
-      result = dfs(stoneSet, stepCount + 1, currentStep + stepCount + 1, lastStep, dp);
-      if (result) {
-        dp.put(key, true);
-        return true;
-      }
-    }
-    dp.put(key, false);
-    return false;
-  }
-
-  private boolean dfsCodeRefined(
-    Set<Integer> stoneSet,
-    int stepCount,
-    int currentStep,
-    int lastStep,
-    Map<String, Boolean> dp
-  ) {
-    String key = stepCount + "" + currentStep;
-    if (dp.containsKey(key)) {
-      return dp.get(key);
-    }
-    if (currentStep == lastStep) {
-      return true;
-    }
-    boolean result;
-    //@formatter:off
+    private boolean dfsCodeRefined(
+            Set<Integer> stoneSet,
+            int stepCount,
+            int currentStep,
+            int lastStep,
+            Map<String, Boolean> dp
+    ) {
+        String key = stepCount + "" + currentStep;
+        if (dp.containsKey(key)) {
+            return dp.get(key);
+        }
+        if (currentStep == lastStep) {
+            return true;
+        }
+        boolean result;
+        //@formatter:off
     if ((stepCount - 1) > 0
       && stoneSet.contains(currentStep + (stepCount - 1))
       && dfsCodeRefined(stoneSet, stepCount - 1, currentStep + (stepCount - 1), lastStep, dp)) {
@@ -86,17 +86,17 @@ public class FrogJumpLeetCode {
         return true;
     }
     //@formatter:on
-    dp.put(key, false);
-    return false;
-  }
+        dp.put(key, false);
+        return false;
+    }
 
 
-  public static void main(String[] args) {
-    //    int[] stones = { 0, 1, 3, 5, 6, 8, 12, 17 };
-    int[] stones = { 0, 2, 4, 5, 6, 8, 9, 11, 14, 17, 18, 19, 20, 22, 23, 24, 25, 27, 30 };
-    //int[] stones = { 0, 2 };
-    FrogJumpLeetCode frogJumpLeetCode = new FrogJumpLeetCode();
-    System.out.println(frogJumpLeetCode.canCross(stones));
-  }
+    public static void main(String[] args) {
+        //    int[] stones = { 0, 1, 3, 5, 6, 8, 12, 17 };
+        int[] stones = {0, 2, 4, 5, 6, 8, 9, 11, 14, 17, 18, 19, 20, 22, 23, 24, 25, 27, 30};
+        //int[] stones = { 0, 2 };
+        FrogJumpLeetCode frogJumpLeetCode = new FrogJumpLeetCode();
+        System.out.println(frogJumpLeetCode.canCross(stones));
+    }
 
 }
